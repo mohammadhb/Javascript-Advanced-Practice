985e1d9a7008de637a4efda023beaf011a77d0d4
Mohammad Hosein Balkhani <mohammadhb25@gmail.com>
2023-09-06 20:45:29 +0330

    Update Builder.js

--- Design Patterns/Builder.js
+++ Design Patterns/Builder.js
@@ -1,4 +1,83 @@
 //ES6+
+class Shop{
+    build(builder) {
+        builder.step1();
+        builder.step2();
+        return builder.get();
+    }
+}
+
+class Car {
+    doors = 0;
+    constructor(){}
+    
+    addParts() {
+        this.doors = 4;
+    };
+    say() {
+        console.log("I am a " + this.doors + "-door car");
+    };
+}
+
+class Truck {
+    doors = 0;
+    constructor(){}
+    
+    addParts() {
+        this.doors = 2;
+    };
+    say() {
+        console.log("I am a " + this.doors + "-door truck");
+    };
+}
+
+class CarBuilder {
+
+    car = null;
+    constructor(){}
+    
+    step1() {
+        this.car = new Car();
+    };
+
+    step2() {
+        this.car.addParts();
+    };
+
+    get() {
+        return this.car;
+    };
+}
+
+class TruckBuilder {
+
+    truck = null;
+    constructor(){}
+    
+    step1() {
+        this.truck = new Truck();
+    };
+
+    step2() {
+        this.truck.addParts();
+    };
+
+    get() {
+        return this.truck;
+    };
+}
+
+function run() {
+    var shop = new Shop();
+    var carBuilder = new CarBuilder();
+    var truckBuilder = new TruckBuilder();
+    var car = shop.build(carBuilder);
+    var truck = shop.build(truckBuilder);
+
+    car.say();
+    truck.say();
+}
+
 
 //ES5
 function Shop() {
