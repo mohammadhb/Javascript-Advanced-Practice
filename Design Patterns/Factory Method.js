7cb457ecf2a21b754e561ac9abb599abcb119bfc
Mohammad Hosein Balkhani <mohammadhb25@gmail.com>
2023-09-06 20:51:42 +0330

    Update Factory Method.js

--- Design Patterns/Factory Method.js
+++ Design Patterns/Factory Method.js
@@ -1,1 +1,106 @@
+//ES6+
+class Factory{
+    createEmployee(type) {
+        var employee;
 
+        if (type === "fulltime") {
+            employee = new FullTime();
+        } else if (type === "parttime") {
+            employee = new PartTime();
+        } else if (type === "temporary") {
+            employee = new Temporary();
+        } else if (type === "contractor") {
+            employee = new Contractor();
+        }
+
+        employee.type = type;
+
+        employee.say = function () {
+            console.log(this.type + ": rate " + this.hourly + "/hour");
+        }
+
+        return employee;
+    }
+}
+
+class FullTime{
+    hourly = "$12";
+};
+
+class PartTime{
+    hourly = "$11";
+};
+
+class Temporary{
+    hourly = "$10";
+};
+
+class Contractor{
+    hourly = "$15";
+};
+
+function run() {
+
+    let factory = new Factory();
+  
+    const employeeTypes = ["fulltime", "parttime", "temporary", "contractor"];
+    employeeTypes
+      .map(employeeType=>factory.createEmployee(employeeType))
+      .forEach(employee=>employees.say());
+}
+
+//ES5
+var Factory = function () {
+    this.createEmployee = function (type) {
+        var employee;
+
+        if (type === "fulltime") {
+            employee = new FullTime();
+        } else if (type === "parttime") {
+            employee = new PartTime();
+        } else if (type === "temporary") {
+            employee = new Temporary();
+        } else if (type === "contractor") {
+            employee = new Contractor();
+        }
+
+        employee.type = type;
+
+        employee.say = function () {
+            console.log(this.type + ": rate " + this.hourly + "/hour");
+        }
+
+        return employee;
+    }
+}
+
+var FullTime = function () {
+    this.hourly = "$12";
+};
+
+var PartTime = function () {
+    this.hourly = "$11";
+};
+
+var Temporary = function () {
+    this.hourly = "$10";
+};
+
+var Contractor = function () {
+    this.hourly = "$15";
+};
+
+function run() {
+
+    var employees = [];
+    var factory = new Factory();
+
+    employees.push(factory.createEmployee("fulltime"));
+    employees.push(factory.createEmployee("parttime"));
+    employees.push(factory.createEmployee("temporary"));
+    employees.push(factory.createEmployee("contractor"));
+
+    for (var i = 0, len = employees.length; i < len; i++) {
+        employees[i].say();
+    }
+}
